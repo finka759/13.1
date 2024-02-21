dd0d779c3bbc92c974a800b6dd81e900ba745ae6
finka759 <finka759@gmail.com>
2024-02-21 21:05:08 +0300

    14.1_1, 14.1_2, 14.1_3  к сдаче окончательно

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
 import json
+import os
 
 from src.category import Category
-from src.product import Product
 
 
 def main():
     list_data = get_products_data()
-    print(list_data)
     objects_list = make_objects_list_from_list(list_data)
-    print(objects_list[0].products[0].name)
+    print(objects_list[0].products)
+    print(objects_list[1].products)
 
 
 def get_products_data():
-    with open('../sourses/products_data.json', encoding='UTF-8') as products_data:
+    way_to_json = os.path.join('..', 'sourses', 'products_data.json')
+    with open(way_to_json, encoding='UTF-8') as products_data:
         data = json.load(products_data)
         return data
 
@@ -22,8 +23,7 @@ def make_objects_list_from_list(list_data):
     for category in list_data:
         list_obj_products = []
         for product in category['products']:
-            list_obj_products.append(
-                Product(product['name'], product['description'], product['price'], product['quantity']))
+            list_obj_products.append(product)
         list_ctgrs.append(Category(category['name'], category['description'], list_obj_products))
     return list_ctgrs
 
--- tests/test_category.py
+++ tests/test_category.py
@@ -23,7 +23,8 @@ def class_category():
 
 def test_init(class_category):
     assert class_category.name == 'Смартфоны'
-    assert class_category.description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
+    assert class_category.description == ('Смартфоны, как средство не только коммуникации, но и получение '
+                                          'дополнительных функций для удобства жизни')
 
     assert class_category.number_of_categories == 1
     assert class_category.number_of_unique_products == 2
@@ -32,6 +33,7 @@ def test_init(class_category):
 def test_products(class_category):
     assert str(class_category.products) == (
         'Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт.\nIphone 15, 210000.0 руб. Остаток: 8 шт.')
+
 
 def test___str__(class_category):
     assert str(class_category) == 'Смартфоны, количество продуктов: 13 шт.'
